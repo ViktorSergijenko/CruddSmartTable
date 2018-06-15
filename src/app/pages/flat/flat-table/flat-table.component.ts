e4da8c1ba1c4a13e9d518e7cf4586be94cfb5241
ViktorSergijenko <viktor.sergijenko@bbit.tech>
2018-06-15 14:06:14 -0700

    smart flat table now have crud fucntions aswell

--- src/app/pages/flat/flat-table/flat-table.component.ts
+++ src/app/pages/flat/flat-table/flat-table.component.ts
@@ -19,21 +19,19 @@ settings = {
     addButtonContent: '<i class="nb-plus"></i>',
     createButtonContent: '<i class="nb-checkmark"></i>',
     cancelButtonContent: '<i class="nb-close"></i>',
+    confirmCreate: true,
   },
   edit: {
     editButtonContent: '<i class="nb-edit"></i>',
     saveButtonContent: '<i class="nb-checkmark"></i>',
     cancelButtonContent: '<i class="nb-close"></i>',
+    confirmSave: true,
   },
   delete: {
     deleteButtonContent: '<i class="nb-trash"></i>',
     confirmDelete: true,
   },
   columns: {
-    id: {
-      title: 'ID',
-      type: 'number',
-    },
     floor: {
       title: 'Floor',
       type: 'number',
@@ -68,14 +66,65 @@ source: LocalDataSource = new LocalDataSource();
     console.log(resp.json());
     this.flatService.flatList = resp.json();
     this.source.load(flatService.flatList);
-    })
+    });
+
 
   }
-  onDeleteConfirm(event): void {
-    if (window.confirm('Are you sure you want to delete?')) {
-      event.confirm.resolve();
-    } else {
-      event.confirm.reject();
-    }
-  }
+ /**
+ *If user will confirm that he wants to delete additional resident,
+ *then this function will call "deleteResident" fucntion that will make a delete request
+ *
+ * @param {*} event - event-Object, consist of:
+data: Object - original row data
+newData: Object - edited data
+source: DataSource - table data source
+confirm: Deferred - Deferred object with resolve(newData: Object) and reject() methods
+ * @memberof ResidentTableComponent ResidentTableComponent - Have all setting of our resident smart table
+ */
+onDeleteConfirm(event): void {
+  this.flatService.deleteFlat(event);
 }
+/**
+ *If user will confirm that he wants to add a new resident,function will call
+ *"postResident" function that will make a post request to other localhost
+ * @param {*} event event-Object, consist of:
+data: Object - original row data
+newData: Object - edited data
+source: DataSource - table data source
+confirm: Deferred - Deferred object with resolve(newData: Object) and reject() methods
+ * @memberof ResidentTableComponent ResidentTableComponent - Have all setting of our resident smart table
+ */
+onCreateConfirm(event): void {
+  const data = {
+    'id' : event.newData.id = 0,
+    'number' : event.newData.number,
+    'totalarea' : event.newData.totalarea,
+    'livingspace' :  event.newData.livingspace,
+    'phone' : event.newData.phone,
+    'houseid' : event.newData.houseid,
+  };
+  this.flatService.postFlat(event, data);
+}
+/**
+*If user will confirm that he wants to change information about additional resident
+* function will call other function called "putResident",that will send put request to our backend
+* @param {*} event event-Object, consist of:
+data: Object - original row data
+newData: Object - edited data
+source: DataSource - table data source
+confirm: Deferred - Deferred object with resolve(newData: Object) and reject() methods
+* @memberof ResidentTableComponent ResidentTableComponent - Have all setting of our resident smart table
+*/
+onSaveConfirm(event): void {
+  const data = {
+    'id' : event.newData.id,
+    'number' : event.newData.number,
+    'totalarea' : event.newData.totalarea,
+    'livingspace' :  event.newData.livingspace,
+    'phone' : event.newData.phone,
+    'houseid' : event.newData.houseid,
+  };
+  this.flatService.putFlat(event, data);
+}
+}
+
--- src/app/pages/flat/flat.service.ts
+++ src/app/pages/flat/flat.service.ts
@@ -11,46 +11,61 @@ export class FlatService {
     flatList: Flat[];
     constructor(private http: Http) { }
     /**
-     * this function addes a new object to our database that is
-     * located in our backedn(other localhost that we have)
-     * @param {Flat} flat this param inheritance(наследует) our house model(class)
-     * @returns returns a new object to our localhost where is located our database.
-     * @memberof FlatService Service that contains all RESTfull functions that we need
-     */
-    postFlat(flat: Flat) {
-        const body = JSON.stringify(flat); // why i cant use var and let instead of const here?
-        const headerOptions = new Headers({ 'Content-Type': 'application/json' });
-        const requestOptions = new RequestOptions({ method: RequestMethod.Post, headers: headerOptions });
-        return this.http.post('http://localhost:52414/api/Flat', body, requestOptions).map(x => x.json());
+    *this function addes a new  object to our databse that is located on our backend.
+    *Function sends a post request to other local server(backend),sends a new object that has to be added
+    * @param {*} event Object
+    * @param {*} data Object - original row data
+    * @memberof FlatService Service that contains all RESTfull functions that we need
+    */
+   postFlat(event, data) {
+    if (window.confirm('Are you sure you want to add a Flat?')) {
+      this.http.post('http://localhost:52414/api/Flat', data).subscribe(res => {
+        console.log(res);
+        event.confirm.resolve(event.newData);
+      });
+      } else {
+        event.confirm.reject();
       }
-      /**
-       * function that moddifies our existed objects that we have in our database,saves all this
-       * changes and sends them back to our backend,where it will be saved in database
-       * @param {any} id we need this param (id) so we could moddefie the exact object that we need(all objects have their unique id)
-       * @param {any} flat "flat" is a param that contains all our class fields that are initializated in our form
-       * @returns returns  moddified object and sends it back tou our localhost(our backend) where is located our database.
-       * @memberof FlatService Service that contains all RESTfull functions that we need
-       */
-      putFlat(id, flat) {
-        const body = JSON.stringify(flat); // why i cant use var and let instead of const here?
-        const headerOptions = new Headers({ 'Content-Type': 'application/json' });
-        const requestOptions = new RequestOptions({ method: RequestMethod.Put, headers: headerOptions });
-        return this.http.put('http://localhost:52414/api/Flat/' + id, body, requestOptions).map(x => x.json());
+  }
+ /**
+  *this function saves all changes with our object(Resident),function sends a put request to
+  *our database that is located on backend,it sends a new information about resident to chnage information about him
+  * @param {*} event its object
+  * @param {*} data Object - original row data
+  * @memberof FlatService Service that contains all RESTfull functions that we need
+  */
+ putFlat(event, data) {
+    if (window.confirm('Are you sure you want to update info about a Flat?')) {
+      this.http.put('http://localhost:52414/api/Flat/' + event.newData.id, data).subscribe(res => {
+        console.log(res);
+        event.confirm.resolve(event.newData);
+      });
+      } else {
+        event.confirm.reject();
       }
-      /**
-       * Function sends a get request to our backend,and returns all data.(in our case it is Flat array)
-       * @memberof FlatService Service that contains all RESTfull functions that we need
-       */
-      getFlatList() {
-        return this.http.get('http://localhost:52414/api/Flat');
-      }
-      /**
-       * function that will delete the selected flat object
-       * @param {number} id id of an object that we want to delete
-       * @returns returns a http request that requests to delete the object with following id.
-       * @memberof FlatService Service that contains all RESTfull functions that we need
-       */
-      deleteFlat(id: number) {
-        return this.http.delete('http://localhost:52414/api/Flat/' + id).map(res => res.json());
-      }
+  }
+  /**
+   * Function sends a get request to our backend,and returns all data.(in our case it is Flat array)
+   * @memberof ResidentService Service that contains all RESTfull functions that we need
+   */
+  getFlatList() {
+    return this.http.get('http://localhost:52414/api/Flat');
+  }
+ /**
+  *function sends a delete request on our (backend) to delete a object that user wants to delete
+  *
+  * @param {*} event Object
+  * @memberof FlatService Service that contains all RESTfull functions that we need
+  */
+ deleteFlat(event) {
+    if (window.confirm('Are you sure you want to delete?')) {
+      console.log(event.data);
+      this.http.delete('http://localhost:52414/api/Flat/' + event.data.id).subscribe(res => {
+      console.log(res);
+      event.confirm.resolve(event.source.data);
+    });
+    } else {
+      event.confirm.reject();
+    }
+  }
 }
